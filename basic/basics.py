de9b0d0bed134864086d8f03d1c83bc0159697f3
fresh <1326191992@qq.com>
2021-09-21 22:45:48 +0800

    1

---
+++ basic/basics.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# basic py
+import sys
+
+vps = 'vps 中'
+
+sys.path = 1
+# print(sys.path)
+
---
+++ chapter1-3/chapter1-3.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+import sys
+import basic.basics
+
+
+print(basic.basics.vps)
+basic.basics.vps = "修改"
+print(basic.basics.vps)
+print(sys.path)
+
