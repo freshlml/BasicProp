01c7f94d7726a9a7c916858563d9aa5a2e2b34e5
fresh <1326191992@qq.com>
2021-11-06 03:13:47 +0800

    class list的Iterator的实现原理，range函数、enumerate函数、zip函数、filter函数

--- basic/chapter3/chapter3-14.py
+++ basic/chapter3/chapter3-14.py
@@ -12,12 +12,6 @@ from collections.abc import Iterator
 #           迭代器对象调用__next__方法不断迭代自身，在一系列迭代之后到达迭代器对象尾部，若再次调用.__next__方法，则触发StopIteration异常
 
 
-# class range(object)定义了__iter__方法，没有定义__next__方法
-rg = range(5)
-print(isinstance(rg, Iterable))  # True
-print(isinstance(rg, Iterator))  # False
-print(isinstance(rg.__iter__(), Iterator))  # True
-
 # class list(object)定义了__iter__方法，没有定义__next__方法
 lst = []
 print(isinstance(lst, Iterable))  # True
@@ -27,6 +21,7 @@ print(lst.__iter__())  # <list_iterator object at 0x00000194340C5710>
 print(isinstance(lst.__iter__(), Iterator))  # True, list_iterator是Iterator
 
 # str、tuple、dict、set这些类型均定义了__iter__方法，没有定义__next__方法
+print("---------1.1--------")
 
 # _io./class TextIOWrapper(_TextIOBase)定义了__next__方法，基类_IOBase定义了__iter__方法
 tm = open("tm", 'r', encoding="utf-8")
@@ -34,7 +29,65 @@ print(isinstance(tm, Iterable))  # True
 print(isinstance(tm, Iterator))  # True
 print(tm.__iter__() is tm)       # True, 返回自身
 
-print("---------1--------")
+print("---------1.2--------")
+
+# class list的Iterator: list_iterator
+lst_n = ['1']
+lst = [1, lst_n]
+# class list的__iter__方法每调用一次，"新构造"一个list的Iterator返回
+lst_iter1 = iter(lst)
+lst_iter2 = lst.__iter__()
+print(lst_iter1 is lst_iter2)  # False
+
+# class list的Iterator在构造时，保存list的引用到成员变量如lst，同时初始化一个迭代下标如index=0，并没有遍历list中每一个元素，拷贝每一个元素的引用值到新创建的列表
+# list的Iterator每次迭代时(调用__next__方法)，使用lst[index]获取元素，同时index=index+1
+print(lst_iter1.__next__())        # 1
+print(lst_iter2.__next__())        # 1
+
+# lst.pop(1)  # 如果这条语句去掉注释，下面的语句将报StopIteration
+
+p1 = lst_iter1.__next__()
+print(p1)         # ['1']
+
+lst_n[0] = '2'
+
+p2 = lst_iter2.__next__()
+print(p2)         # ['2']
+print(p1 is p2)   # True
+
+# str、tuple、dict、set这些类型和list的迭代器逻辑一致
+print("---------1.3--------")
+
+# class range, 定义了__iter__,没有定义__next__
+rg = range(3)
+print(isinstance(rg, Iterable))  # True
+print(isinstance(rg, Iterator))  # False
+# class range的__iter__方法每调用一次，"新构造"一个list的Iterator返回
+# class range的Iterator在构造时，保存range的step和stop值，同时初始化一个迭代下标如index=0
+rg_iter1 = iter(rg)
+rg_iter2 = iter(rg)
+print(next(rg_iter1))  # 0
+print(next(rg_iter2))  # 0
+
+print("---------1.4--------")
+
+# enumerate、zip、filter
+# 定义了__iter__(返回自身，其实还有一种实现，返回构造时的Iterable.__iter__但充满歧义),定义__next__
+# 节省内存空间 -->  构造时没有对参数迭代进而拷贝引用值
+lst = [1, 2]
+ft = filter(None, lst)
+print(isinstance(ft, Iterator))  # True
+print(ft.__iter__() is ft)       # True，filter的__iter__返回自身
+one = next(ft)
+two = next(ft)
+# when others concurrency add element to lst, three will not be StopIteration
+lst.append('3')
+three = next(ft)  # StopIteration or not
+
+# 构造器filter(None, lst)执行时，调用list.__iter__方法创建list的Iterator保存，并没有遍历list中每一个元素，拷贝每一个元素的引用值到新创建的列表
+#  当对filter迭代(调用__next__方法)时，使用list的Iterator迭代
+
+print("------1.5------")
 
 
 class B(object):
@@ -55,7 +108,7 @@ class A(object):
 
 # for param in Iterable的逻辑代码:
 #  1、nextOrIterator = Iterable.__iter__()
-#  2、nextOrIterator.__next__()迭代
+#  2、nextOrIterator.__next__()
 
 # for x in B():  # 报错, 'B' is not iterable
 #    pass
