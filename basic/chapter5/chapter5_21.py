34e6fa7a37d887d771d65b20959a6ec5768e7934
fresh <1326191992@qq.com>
2022-02-17 23:08:00 +0800

    包导入，相对路径

--- basic/chapter5/chapter5_21.py
+++ basic/chapter5/chapter5_21.py
@@ -10,7 +10,6 @@ import sys
 # 3. 将module的源代码编译成字节码或者直接使用已存在的字节码
 # 4. 执行module的字节码，生成class module对象，赋值给此中变量
 import chapter5_21_1
-import chapter5_21_2
 
 # 打印"搜索路径"，TODO，一些目录需要认识
 print(sys.path)
@@ -24,6 +23,38 @@ print(type(chapter5_21_1))  # <class 'module'>
 # dir()函数,__dir__()协议方法
 print(dir(chapter5_21_1))
 print(chapter5_21_1.__dir__())
+
+print("-------2-------")
+
+# 在搜索路径中查找 sub目录的chapter5_sub
+import sub.chapter5_sub
+# import sub.chapter5_sub as chapter5_sub
+print(sub.chapter5_sub.sub)
+print(type(sub))  # <class 'module'>, sub目录被解析成class module，对应该目录下的__init__.py文件,包首次被读取时执行
+print(type(sub.chapter5_sub))  # <class 'module'>
+print(sub is sub.chapter5_sub)  # False
+
+from sub import chapter5_sub_from
+print(chapter5_sub_from.sub_from)
+
+print("--------3------")
+
+# 规范写法?
+from basic.chapter5.pol import qq
+import basic.chapter5.pol.ww as ww
+print(qq.var)
+print(ww.var)
+
+# 在相对路径中查找,.表示当前目录，..表示上层目录
+from .rel import chapter5_21_rel
+from .. import basics
+
+
+
+
+
+
+
 
 
 
