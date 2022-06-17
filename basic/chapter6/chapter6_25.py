867611ea17d2cf18c608f725e6b5e59f870ae294
freshlml <1326191992@qq.com>
2022-06-17 17:12:34 +0800

    1

--- basic/chapter6/chapter6_25.py
+++ basic/chapter6/chapter6_25.py
@@ -36,8 +36,8 @@ class A(One, Two):
 
 print(A.mro())  # A One T Two object
 # python中，可以从两个角度理解如下语句
-# 1. 面向对象角度: 构造A类的实例对象并显示调用A.__init__协议方法
-# 2. python的机制角度: 隐式触发__call__协议方法
+# 1. 面向对象角度: 构造A类的实例对象并(显示)调用A.__init__协议方法
+# 2. python的机制角度: (隐式)触发__call__协议方法
 a = A("参数")  # A.__init__方法搜到One的
 print(a.one_sl_attr)  # 参数
 # print(a.two_sl_attr)  # AttributeError: 'A' object has no attribute 'two_sl_attr'
--- basic/chapter6/chapter6_26.py
+++ basic/chapter6/chapter6_26.py
@@ -79,16 +79,16 @@ A.m(None)         # e_m, 在A类上搜索方法, A类的mro路径 + (A类作为�
 A.m1()           # a_meta_m, 在A类上调用方法的self约定: (A类作为实例对象)A.__class__的mro路径 中搜索到的方法将自身作为第一个参数
 print("------------1------------")
 # python中，可以从两个角度理解如下语句
-# 1. 面向对象角度: 构造A类的实例对象并显示调用A.__init__协议方法
-# 2. python机制角度: 隐式触发__call__协议方法
-a = A()        # a_meta_call, 对A类的调用隐式触发__call__协议方法，从(A类作为实例对象)A.__class__的mro路径 中搜索
-A.__call__(None)  # e_call, A类显示调用__call__协议方法，和普通方法搜索规则一致
+# 1. 面向对象角度: 构造A类的实例对象并(显示)调用A.__init__协议方法
+# 2. python机制角度: (隐式)触发__call__协议方法
+a = A()        # a_meta_call, 对A类的调用(隐式)触发__call__协议方法，从(A类作为实例对象)A.__class__的mro路径 中搜索
+A.__call__(None)  # e_call, A类(显示)调用__call__协议方法，和普通方法搜索规则一致
 print(a.attr)  # e, A类的实例对象上搜索属性, 实例对象 + 实例对象.__class__的mro路径
 a.m()          # 1, A类的实例对象上搜索方法, 实例对象 + 实例对象.__class__的mro路径
 print()        # ,  A类的实例对象上调用方法的self约定: 实例对象.__class__的mro路径 中搜索到的方法将自身作为第一个参数
 a.__call__ = lambda *args: print("a_call")
-a()            # e_call, A类的实例对象的调用隐式触发__call__协议方法，从实例对象.__class__的mro路径 中搜索
-a.__call__()   # a_call, A类的实例对象显示嗲用__call__协议方法
+a()            # e_call, A类的实例对象的调用(隐式)触发__call__协议方法，从实例对象.__class__的mro路径 中搜索
+a.__call__()   # a_call, A类的实例对象(显示)调用__call__协议方法
 print("------------2------------")
 
 
