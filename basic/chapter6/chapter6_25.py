708fa274c8832aff8d04294ef4ac32279e7b8c99
freshlml <1326191992@qq.com>
2022-06-17 13:23:28 +0800

    规范说法

--- basic/chapter6/chapter6_25.py
+++ basic/chapter6/chapter6_25.py
@@ -35,7 +35,10 @@ class A(One, Two):
 
 
 print(A.mro())  # A One T Two object
-a = A("参数")  # 构造A类实例对象，调用__init__方法: a.__init__("参数")方法搜到One的
+# python中，可以从两个角度理解如下语句
+# 1. 面向对象角度: 构造A类的实例对象并调用A.__init__方法
+# 2. python的机制角度: __call__协议方法
+a = A("参数")  # A.__init__方法搜到One的
 print(a.one_sl_attr)  # 参数
 # print(a.two_sl_attr)  # AttributeError: 'A' object has no attribute 'two_sl_attr'
 # print(a.t_sl_attr)  # AttributeError: 'A' object has no attribute 't_sl_attr'
--- basic/chapter6/chapter6_26.py
+++ basic/chapter6/chapter6_26.py
@@ -73,20 +73,23 @@ class A(B, C, metaclass=AMeta):
 
 # print(AMeta.mro())
 print(A.mro())  # A B E C D F object
-print(A.attr)    # e,   在类A上搜索属性, 类A的mro路径 + (类A作为实例对象)类A.__class__的mro路径
+print(A.attr)    # e,   在A类上搜索属性, A类的mro路径 + (A类作为实例对象)A.__class__的mro路径
 print(A.attr1)   # a_meta
-A.m(None)         # e_m, 在类A上搜索方法, 类A的mro路径 + (类A作为实例对象)类A.__class__的mro路径
-A.m1()           # a_meta_m, 在类A上调用方法的self约定: (类A作为实例对象)类A.__class__的mro路径 中搜索到的方法将自身作为第一个参数
+A.m(None)         # e_m, 在A类上搜索方法, A类的mro路径 + (A类作为实例对象)A.__class__的mro路径
+A.m1()           # a_meta_m, 在A类上调用方法的self约定: (A类作为实例对象)A.__class__的mro路径 中搜索到的方法将自身作为第一个参数
 print("------------1------------")
-a = A()        # a_meta_call, 对类A的调用，__call__协议方法，从(类A作为实例对象)类A.__class__的mro路径 中搜索
-print(a.attr)  # e, 类A的实例对象上搜索属性, 实例对象 + 实例对象.__class__的mro路径
-a.m()          # 1, 类A的实例对象上搜索方法, 实例对象 + 实例对象.__class__的mro路径
-print()        # ,  类A的实例对象上调用方法的self约定: 实例对象.__class__的mro路径 中搜索到的方法将自身作为第一个参数
+# python中，可以从两个角度理解如下语句
+# 1. 面向对象角度: 构造A类的实例对象并调用A.__init__方法
+# 2. python的机制角度: __call__协议方法
+a = A()        # a_meta_call, 对A类的调用，__call__协议方法，从(A类作为实例对象)A.__class__的mro路径 中搜索
+print(a.attr)  # e, A类的实例对象上搜索属性, 实例对象 + 实例对象.__class__的mro路径
+a.m()          # 1, A类的实例对象上搜索方法, 实例对象 + 实例对象.__class__的mro路径
+print()        # ,  A类的实例对象上调用方法的self约定: 实例对象.__class__的mro路径 中搜索到的方法将自身作为第一个参数
 a.__call__ = lambda self, *args: print("a_call")
-a()            # e_call, 类A的实例对象的调用，__call__协议方法，从实例对象.__class__的mro路径 中搜索
+a()            # e_call, A类的实例对象的调用，__call__协议方法，从实例对象.__class__的mro路径 中搜索
 print("------------2------------")
 a2 = A()
-A.__init__(a2)  # __init__方法的搜索和普通方法一样: 类A的mro路径 + (类A作为实例对象)类A.__class__的mro路径
+A.__init__(a2)  # __init__方法的搜索和普通方法一样: A类的mro路径 + (A类作为实例对象)A.__class__的mro路径
 a2.__init__ = lambda self: print("a2_init")
 a2.__init__(None)  # a2_init, __init__方法的搜索和普通方法一样: 实例对象 + 实例对象.__class__的mro路径
 
