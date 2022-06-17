f5ef848c636e5effd8830917f89d3eafd0ce6c22
freshlml <1326191992@qq.com>
2022-06-17 18:54:38 +0800

    1

--- basic/chapter6/chapter6_30.py
+++ basic/chapter6/chapter6_30.py
@@ -13,7 +13,7 @@ print(a.m())  # <__main__.A object at 0x000001A62C68FDA0>, a作为第一个参�
 
 
 # 类中方法定义按照约定，第一个参数应该定义成self，并且self约定为当前类(或子类)的实例对象
-# python中有静态方法的概念: 使实例对象调用时不会把自身作为第一个参数
+# python中有静态方法的概念: 使任何对象调用静态方法时都不会把自身作为第一个参数
 class B(object):
     attr = 1
 
@@ -23,10 +23,10 @@ class B(object):
         return param
 
 
-print(B.m("参数"))  # 参数, 类调用静态方法
+print(B.m("参数"))  # 参数
 print(B.attr)  # 2
 b = B()
-print(b.m("参数2"))  # 参数2, B.m("参数2"), 不会把实例对象b作为第一个参数
+print(b.m("参数2"))  # 参数2, 因为静态方法的缘故, b不会将自身作为第一个参数
 print(B.attr)  # 3
 
 
@@ -46,20 +46,20 @@ class C(object):
 # 类/对象引用静态方法
 c_s_m = C.s_m
 print(c_s_m)  # <function C.s_m at 0x00000277FCF34C80>
-c_s_m("类引用静态方法")
+c_s_m("类引用静态方法")  # the same as C.s_m("类引用静态方法")
 llc = C()
 ll_m = llc.s_m
 print(ll_m)  # <function C.s_m at 0x0000021F73E54C80>
-ll_m("对象引用静态方法")
+ll_m("对象引用静态方法")  # the same as llc.s_m("类引用静态方法")
 # 类引用
 c_m = C.m
 print(c_m)  # <function C.m at 0x000001CA5D234BF8>
-c_m(C(), "类引用")
+c_m(C(), "类引用")  # the same as C.m(C(), "类引用")
 # 对象引用
 lc = C()
 l_m = lc.m
 print(l_m)  # <bound method C.m of <__main__.C object at 0x000001CA5D230B00>>
-l_m("对象引用")  # lc.m("对象引用")
+l_m("对象引用")  # the same as lc.m("对象引用")
 print(l_m.__self__)  # <__main__.C object at 0x0000023CC1850AC8>
 print(l_m.__self__ is lc)  # True
 print(l_m.__func__)  # <function C.m at 0x0000023CC1854BF8>
