e15a06ee718dad307b97092f7e3d85a836fd0c4c
freshlml <1326191992@qq.com>
2022-06-24 02:52:53 +0800

    日期，时间

--- basic/chapter3/chapter3_13.py
+++ basic/chapter3/chapter3_13.py
@@ -1,5 +1,7 @@
 from collections.abc import Iterable
 from collections.abc import Iterator
+import datetime
+import time
 
 # for param in iterable
 lst = [1, 2, '3', [1, '2']]
@@ -63,6 +65,61 @@ for lst1Param, list2Param in zp:
 print()
 print("------6------")
 
+# date
+date = datetime.date(1009, 2, 3)  # 指定年，月，日构造date
+print(date)  # 1009-02-03
+
+date = datetime.date.today()  # 获取系统默认时区的当前日期
+print(date)  # 2022-06-24
+
+# 57600 = 24*60*60 - 8*60*60
+date = datetime.date.fromtimestamp(57599)  # 根据时间戳和系统默认时区构造date
+print(date)  # 1970-01-01
+date = datetime.date.fromtimestamp(57600)
+print(date)  # 1970-01-02
+
+print("------------date------------")
+
+# datetime timezone
+print(datetime.datetime.now())  # 系统默认时区的当前时间
+print(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))  # +8时区的当前时间
+print(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8, minutes=50))))  # +8时区细分
+print(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=0))))  # UTC时间(0区的当前时间)
+
+print("------------1-----------")
+
+# 时间戳和时区无关
+print(time.time())  # 时间戳
+print(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))).timestamp())  # 时间戳
+print(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8, minutes=50))).timestamp())  # 时间戳
+print(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=0))).timestamp())  # 时间戳
+
+print("------------2-----------")
+
+# 根据时间戳构造datetime时，需要指定时区
+print(datetime.datetime.fromtimestamp(time.time(), datetime.timezone(datetime.timedelta(hours=+8))))
+
+print("------------3-----------")
+
+# 格式化
+print(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))
+
+print("------------4-----------")
+
+# 相等性, datetime存储时区值，相等性比较时进行时区转换
+s = time.time()
+l8 = datetime.datetime.fromtimestamp(s, datetime.timezone(datetime.timedelta(hours=+8)))
+l0 = datetime.datetime.fromtimestamp(s, datetime.timezone(datetime.timedelta(hours=0)))
+print(l8)
+print(l0)
+print(l8 == l0)  # True
+
+'''
+时区时间的图形化
+ |---------|      UTC(+0)
+    |----------|  +8
+
+'''
 
 
 
