4c61710c5132685bb92093093d3843bb513542bb
freshlml <1326191992@qq.com>
2022-08-11 23:21:37 +0800

    属性搜索，属性设置，当属性是Descriptor时的情况

--- basic/chapter6/chapter6_29_2.py
+++ basic/chapter6/chapter6_29_2.py
@@ -34,8 +34,11 @@ print("--------------1----------------")
 class Name(object):
     def __get__(self, instance, owner):
         if instance is not None:
-            return instance.cap_name
-        return owner.cap_name
+            try:
+                return instance.cap_name
+            except AttributeError:
+                return None
+        return owner
 
     def __set__(self, instance, v):
         if v:
@@ -45,20 +48,35 @@ class Name(object):
         del instance.cap_name
 
 
-class B(object):
+class C(object):
     name = Name()
 
-    def __init__(self, name):
-        self.cap_name = name
+
+class B(C):
+    pass
 
 
-b = B(1)
-b.name = "任意"  # 翻译成name.__set__(b, "任意")
-print(b.name)  # 翻译成name.__get__(b, b.__class__)
+b = B()
+print(b.__dict__)  # {}
+
+# 如果name属性在b.__class__的mro路径中存在并且name属性是Descriptor with __set__方法，则调用name.__set__(b, '任意')，而不是为b设置name属性
+b.name = "任意"
+print(b.__dict__)  # {'cap_name': '任意'}
+
+# B.name = "覆盖"  # name属性在B的mro路径中存在，而不是B.__class__的mro路径中存在，所以不满足条件，将为B设置name属性
+
+
+# b，b.__class__ or b.__class__的mro?
+# name属性存在并且是Descriptor with __get__方法，调用name.__get__(b, b.__class__) {在b.__class__的mro路径中搜索到name属性}
+print(b.name)  # 任意
+
+# name属性存在并且是Descriptor with __get__方法，调用name.__get__(None, B) {在B的mro路径中搜索到name属性}
+print(B.name)  # class B
+
+# name属性存在并且是Descriptor with __get__方法，调用name.__get__(None, C) {在C的mro路径中搜索到name}
+print(C.name)  # class C
+
 print("---2----------------")
-
-
-# 属性管理: 协议方法 @link chapter6_29_3
 
 
 
