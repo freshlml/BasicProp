6f3402993e491d466c6a87f05776e128e11d4009
freshlml <1326191992@qq.com>
2022-10-15 00:49:26 +0800

    嵌套作用域属性搜索迁移

--- basic/chapter6/chapter6_25.py
+++ basic/chapter6/chapter6_25.py
@@ -43,3 +43,29 @@ print(a.one_sl_attr)  # 参数
 # print(a.two_sl_attr)  # AttributeError: 'A' object has no attribute 'two_sl_attr'
 # print(a.t_sl_attr)  # AttributeError: 'A' object has no attribute 't_sl_attr'
 a.mt("mro")  # T_mro, mt方法搜到T的
+
+
+module_pp = "module_pp"
+
+
+# 嵌套作用域的属性搜索与类声明时的属性定义
+class D(object):
+    class_pp = "class_pp"  # 在类的空间设置属性，不在嵌套作用域的属性搜索范围之内
+    class_pp2 = module_pp
+
+    def d1(self):
+        print(module_pp)  # 类中定义的方法和函数有一样的属性搜索原则: 本def,module,builtins；注意:没有class
+        print(class_pp)  # error
+        print(class_pp2)  # error
+
+    def d2(self):
+        d1(self)  # error
+
+    class DD(object):
+        dd_pp = module_pp
+        dd_pp2 = class_pp  # error
+
+
+d = D()
+d.d1()
+d.d2()
