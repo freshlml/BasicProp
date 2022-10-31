3136f9a9795ece66193471412046258804b98beb
freshlml <1326191992@qq.com>
2022-10-31 22:10:01 +0800

    logging api 1

---
+++ basic/chapter7/chapter7_36.py
@@ -0,0 +1,23 @@
+
+# LogRecord, 一个LogRecord实例对象代表一条日志信息
+
+# Style, Formatter Style
+# Formatter, LogRecord convert to Text
+
+# Filter, Loggers and Handlers can optionally use Filter instances to filter records as desired
+
+# Handler, Handler instances dispatch logging events to specific destinations
+
+# Logger, Instances of the Logger class represent a single logging channel. A
+#     "logging channel" indicates an area of an application. Exactly how an
+#     "area" is defined is up to the application developer. Since an
+#     application can have any number of areas, logging channels are identified
+#     by a unique string
+
+
+
+
+
+
+
+
